d0310ea8622a4aa6485245de95443834af47c7f1
Herg Yang <37422683+Hergyy@users.noreply.github.com>
2022-10-26 17:51:54 +1100

    Update raindrop.js

--- raindrop.js
+++ raindrop.js
@@ -5,31 +5,25 @@ class Raindrop {
   }
 
   reset() {
-    // starts raindrops slightly above canvas
+    // starts raindrops above canvas
     this.x = random(0, width);
     this.y = random(-height, 0);
     this.size = random(1.5, 8); 
     if (this.size > 3) {
       this.size = random(1.5, 8);
     }
-    this.speed = this.size / 0.5; // speed at which raindrop falls
+    // speed of raindrop
+    this.speed = this.size / 0.5; 
     this.dropThickness = floor(this.size / 3.8);
     if (this.dropThickness < 1) {
       this.dropThickness = 1;
     }
   }
 
-  // moves the raindrop down based on it's size & speed, which creates the illusion of debth
+  // raindrops fall according to size
   fall() {
     this.y += this.speed;
-    
-    if (this.y > mouseY && this.x > mouseX - this.block && this.x < mouseX + this.block && this.y < mouseY + 20) { // block condition
-
-      this.splash();
-      this.y = random(-200, -100);
       this.yspeed = map(this.z, 0, 20, 4, 10);
-
-    }
     
   }
 
@@ -41,7 +35,7 @@ class Raindrop {
   }
   
 
-  // returns true if full length of raindrop has exited the bottom of the screen
+  //if raindrop has left the screen
   hasFallen() {
     let answer = false;
     if (this.y - this.size > height) {
@@ -49,16 +43,5 @@ class Raindrop {
     }
     return answer;
   }
-  
-  // splash() {
-  //   stroke(255);
-  //   var thick = map(this.z, 0, 20, 1, 3)
-  //   strokeWeight(thick);
-  //   ellipse(this.x + 3, this.y - 1, 0.5, 0.5);
-  //   ellipse(this.x - 3, this.y - 1, 0.5, 0.5);
-  //   ellipse(this.x + 8, this.y - 3, 0.5, 0.5);
-  //   ellipse(this.x - 8, this.y - 3, 0.5, 0.5);
-
-
-  //} 
 }
+
